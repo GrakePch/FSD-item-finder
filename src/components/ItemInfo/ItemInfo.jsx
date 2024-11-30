ea71db0a8ea4f554da8bbc47f53c7fc8412009b8
GrakePCH <grakepch@gmail.com>
2024-11-30 06:38:10 -0500

    feat: sort buy options

--- src/components/ItemInfo/ItemInfo.jsx
+++ src/components/ItemInfo/ItemInfo.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./ItemInfo.css";
 
 const percent = (v, zero, hundred) => {
@@ -5,11 +6,24 @@ const percent = (v, zero, hundred) => {
 };
 
 const ItemInfo = ({ item }) => {
+  const [sortBy, setSortBy] = useState("price");
+  const [sortDir, setSortDir] = useState(1);
+  const [buyOptions, setBuyOptions] = useState([]);
   const numOptions = item.locations.length;
   const priceMin = item.locations?.[0].price;
   const priceMax = item.locations?.[numOptions - 1].price;
 
   const [type, subType] = item.type.zh.split("/");
+
+  useEffect(() => {
+    let tempBuyOptions = item.locations.toSorted(
+      (a, b) =>
+        (sortBy === "location"
+          ? a.location.en.localeCompare(b.location.en)
+          : a.price - b.price) * sortDir
+    );
+    setBuyOptions(tempBuyOptions);
+  }, [item, sortBy, sortDir]);
 
   return (
     <div className="ItemInfo">
@@ -23,11 +37,35 @@ const ItemInfo = ({ item }) => {
       </div>
       <hr />
       <div className="buy-option-titles">
-        <h4 className="location">购买地点</h4>
-        <h4 className="price">价格</h4>
+        <h4
+          className="location"
+          onClick={() => {
+            if (sortBy === "location") {
+              setSortDir(-1 * sortDir);
+            } else {
+              setSortBy("location");
+              setSortDir(1);
+            }
+          }}
+        >
+          购买地点{sortBy === "location" ? (sortDir > 0 ? " ▲" : " ▼") : " △"}
+        </h4>
+        <h4
+          className="price"
+          onClick={() => {
+            if (sortBy === "price") {
+              setSortDir(-1 * sortDir);
+            } else {
+              setSortBy("price");
+              setSortDir(1);
+            }
+          }}
+        >
+          价格{sortBy === "price" ? (sortDir > 0 ? " ▲" : " ▼") : " △"}
+        </h4>
       </div>
       <div className="buy-option-list">
-        {item.locations.map((option) => {
+        {buyOptions.map((option) => {
           let hue = 200 - percent(option.price, priceMin, priceMax) * 2;
           return (
             <div className="buy-option" key={option.location.en}>
