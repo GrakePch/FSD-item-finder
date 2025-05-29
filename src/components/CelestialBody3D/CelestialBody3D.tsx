8d21f825d35be62445b8c50541049105bc5f9b06
GrakePch <grakepch@gmail.com>
2025-05-29 00:54:11 -0400

    refactor: improve occlusion check in LocationLabel component

--- src/components/CelestialBody3D/CelestialBody3D.tsx
+++ src/components/CelestialBody3D/CelestialBody3D.tsx
@@ -102,7 +102,7 @@ export default function CelestialBody3D({
         {/* Render location labels */}
         {celestialBody.locations &&
           celestialBody.locations.map((loc) => (
-            <LocationLabel loc={loc} key={loc.name} sphereRef={sphereRef} />
+            <LocationLabel loc={loc} key={loc.name} bodyRadius={radius} />
           ))}
         {/* Render Orbital Markers */}
         <OrbitalMarkers radius={radius} color={themeColor} sphereRef={sphereRef} />
--- src/components/CelestialBody3D/LocationLabel.tsx
+++ src/components/CelestialBody3D/LocationLabel.tsx
@@ -7,13 +7,14 @@ import Icon from "@mdi/react";
 import locationIcon from "../../assets/locationIcon";
 import LocationIconColor from "../../assets/locationIconColor";
 import { locationNameToI18nKey, toUrlKey } from "../../utils";
+import { useThree, useFrame } from "@react-three/fiber";
 
 export default function LocationLabel({
   loc,
-  sphereRef,
+  bodyRadius,
 }: {
   loc: SCLocation;
-  sphereRef: React.RefObject<THREE.Mesh>;
+  bodyRadius: number;
 }) {
   const { t } = useTranslation();
   const [occluded, setOccluded] = useState(false);
@@ -26,12 +27,35 @@ export default function LocationLabel({
     loc.type === "Landing zone" ||
     loc.type === "Space station" ||
     loc.type === "Asteroid base";
+
+  const { camera } = useThree();
+
+  // Label position
+  const labelPos = new THREE.Vector3(loc.coordinateX, loc.coordinateZ, -loc.coordinateY);
+
+  // More efficient occlusion check
+  useFrame(() => {
+    // Camera direction (normalized)
+    const dir = new THREE.Vector3();
+    camera.getWorldDirection(dir);
+    // Compute t for closest point on the line to the origin
+    const t = -labelPos.dot(dir);
+    // Closest point on the line
+    const closest = new THREE.Vector3().copy(labelPos).add(dir.clone().multiplyScalar(t));
+    // Distance from origin to closest point
+    const dist = closest.length();
+    // Determine if label is on the back side of the sphere
+    const isBack = dir.dot(labelPos) > 0;
+    // Occluded if distance < sphere radius AND label is on the back
+    const shouldOcclude = dist < bodyRadius && isBack;
+    if (shouldOcclude !== occluded) setOccluded(shouldOcclude);
+  });
+
   return (
     <Html
       position={[loc.coordinateX, loc.coordinateZ, -loc.coordinateY]}
       center
-      occlude={[sphereRef]}
-      onOcclude={setOccluded}
+      occlude={false} /* Implemented a more efficient occlusion check */
       className={`location-label ${occluded ? "occluded" : ""} ${isLarge ? "large" : ""}`}
       zIndexRange={[500, 0]}
     >
